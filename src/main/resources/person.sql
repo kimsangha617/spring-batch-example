4b4d1bcf45cf6354dfe38f7fb0b6f1cdfe09e839
haas <kimsangha617@gmail.com>
2023-07-03 09:12:37 +0900

    JdbcCursorItemReader

---
+++ src/main/resources/person.sql
@@ -0,0 +1,14 @@
+create table person (
+    id bigint primary key auto_increment,
+    name varchar(255),
+    age varchar(255),
+    addr varchar(255)
+);
+
+insert into person (name, age, addr) values('크크크','32','인천');
+
+insert into person (name, age, addr) values('캬캬캬','29','강원');
+
+insert into person (name, age, addr) values('뾲뾲뾲','34','경기');
+
+insert into person (name, age, addr) values('후후후','37','수원');
