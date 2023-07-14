cbb8c9dd4612072751c0e22b689646c0d651e404
haas <kimsangha617@gmail.com>
2023-07-14 09:04:58 +0900

    User Grade project

---
+++ src/main/java/fastcampus/spring/batch/part3/PersonValidationRetryProcessor.java
@@ -0,0 +1,28 @@
+package fastcampus.spring.batch.part3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.RetryListener;
+
+@Slf4j
+public class PersonValidationRetryProcessor {
+
+    public static class SavePersonRetryListener implements RetryListener {
+
+        @Override
+        public <T, E extends Throwable> boolean open(RetryContext context, RetryCallback<T, E> callback) {
+            return true;
+        }
+
+        @Override
+        public <T, E extends Throwable> void close(RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
+            log.info("close");
+        }
+
+        @Override
+        public <T, E extends Throwable> void onError(RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
+            log.info("onError");
+        }
+    }
+}
--- src/main/resources/person.sql
+++ src/main/resources/person.sql
@@ -5,10 +5,10 @@
 --     addr varchar(255)
 -- );
 
-insert into person (name, age, addr) values('크크크','32','인천');
+-- insert into person (name, age, addr) values('크크크','32','인천');
 
-insert into person (name, age, addr) values('캬캬캬','29','강원');
+-- insert into person (name, age, addr) values('캬캬캬','29','강원');
 
-insert into person (name, age, addr) values('뾲뾲뾲','34','경기');
+-- insert into person (name, age, addr) values('뾲뾲뾲','34','경기');
 
-insert into person (name, age, addr) values('후후후','37','수원');
+-- insert into person (name, age, addr) values('후후후','37','수원');
